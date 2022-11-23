405ccc0186050b39335ea34e612f33d8d4238c3d
thasup <thanachonfirst@hotmail.com>
2022-11-24 00:28:38 +0700

    Deployment to Goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const fs = require("fs");
+let mnemonic = fs.readFileSync(".secret").toString().trim();
+let infuraProjectID = fs.readFileSync(".infura").toString().trim();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/" + infuraProjectID,
+      accounts: {
+        mnemonic,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+      },
+    },
+  },
   solidity: "0.8.17",
 };
