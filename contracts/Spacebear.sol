1531e253fc93b64e00fb00a7530b39623ddd28bd
thasup <thanachonfirst@hotmail.com>
2022-11-24 21:08:42 +0700

    Add contract fixtures

--- contracts/Spacebear.sol
+++ contracts/Spacebear.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+import "hardhat/console.sol";
+
 contract Spacebear is ERC721, Ownable {
     using Counters for Counters.Counter;
 
@@ -18,6 +20,7 @@ contract Spacebear is ERC721, Ownable {
 
     function safeMint(address to) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
+        console.log("got here", tokenId);
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
